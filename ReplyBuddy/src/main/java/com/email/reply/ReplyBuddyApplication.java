2fbb251d64734405bc8de46e41620493a6b37615
Satyam57 <satyamprakash5555@gmail.com>
2025-10-05 22:44:34 +0530

    Inital Commit

---
+++ ReplyBuddy/src/main/java/com/email/reply/ReplyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.email.reply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyBuddyApplication.class, args);
+	}
+
+}
---
+++ ReplyBuddy/src/main/java/com/email/reply/app/EmailGeneratorController.java
@@ -0,0 +1,27 @@
+package com.email.reply.app;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
